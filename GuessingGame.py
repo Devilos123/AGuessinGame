1d61cb6bc5676a8c23d53127c3368d2be1b6235b
Devilos123 <shreshyboi.com@gmail.com>
2024-07-31 23:23:58 +0600

    GuessingGame.py

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,10 +1,10 @@
-secret_number = 6
-guess_count = 0
-guess_limit = 3
-while guess_count < guess_limit:
+secret_number = 6 #This is the secret number
+guess_count = 0 #This is the guess count
+guess_limit = 3 #This is how many guesses the player can have.
+while guess_count < guess_limit: #This is the while loop
     guess = int(input("Guess: "))
     guess_count += 1
-    if guess == secret_number:
+    if guess == secret_number: #There is if else or most likely while else.
         print("You won")
         break
 else:    
